d2dcf6d2720604184471121a5555e2d7a6ddf373
Doctoremm <doctoremm@gmail.com>
2018-11-13 08:39:36 -0700

    Renamer fixed

--- Models/Maya_Scripts/Renamer.py
+++ Models/Maya_Scripts/Renamer.py
@@ -1,39 +1,39 @@
 import maya.cmds as cmds
 
 
-def Rename (input):
+def Rename (input, counter):
    
     nam = input 
     #= `GetTextFieldButtonGrpString (input)` 
     new = nam.split ("#")
     #split the input
+    new = filter (None, new)
         
     object = new[0]
     type = new[1]
-    
+    print(type)
     selected = cmds.ls(selection=True)
     #lists selected objects
-    num = 0
-    #counter
 
     hash = len(nam) - (len(object) + len(type))
     # number of ##
-
+    print(len(nam))
+    print(len(object))
+    print (len(type))
     for obj in selected:
         
         pad = ""
-        num +=1
         
         i=1
-        while i<=num:
+        #while i<=str(counter):
             
-            digits = 1
-            digits+=1
+            #digits = 1
+            #digits+=1
             #finding number of digits in selection
 
-            i*=10
+           # i*=10
 
-        dif = hash - digits
+        dif = hash - len(str(counter))
         #difference
 
         i=0
@@ -45,7 +45,7 @@ def Rename (input):
             i+=1
 
         
-        pad += str(num)
+        pad += str(counter)
         #add padding to number
 
 
@@ -53,5 +53,6 @@ def Rename (input):
         #put back together      
         cmds.rename (obj, renamer)
         #renames object
+        counter+=1
 
-Rename("L_Arm_#_Jnt")
+Rename("L_Arm_###_Jnt",1)

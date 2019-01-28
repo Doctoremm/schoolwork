b82e70be754eafb2398431f2978155d2193d2c8c
Dareah <doctoremm@gmail.com>
2019-01-28 12:50:11 -0700

    level stuff

--- Models/Projects/Level Design/Level Design/Assets/Scripts/ThirdPersonCameraController.cs
+++ Models/Projects/Level Design/Level Design/Assets/Scripts/ThirdPersonCameraController.cs
@@ -21,7 +21,7 @@ public class ThirdPersonCameraController : MonoBehaviour {
 	void CamControl()
 	{
 		mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
-		mouseY -= Input.GetAxis("MouseY") * rotationSpeed;
+		mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
 		mouseY = Mathf.Clamp(mouseY, -35, 60);
 
 		transform.LookAt(target);
--- Models/Projects/Level Design/Level Design/Assets/Scripts/ThirdPersonCharacterController.cs
+++ Models/Projects/Level Design/Level Design/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ThirdPersonCharacterController : MonoBehaviour {
 
 	public float speed = 3f;
+    public Rigidbody rb;
+    public float jump = 10f;
 
 	void Update()
 	{
@@ -15,6 +17,11 @@ public class ThirdPersonCharacterController : MonoBehaviour {
 	{
 		float hor = Input.GetAxis("Horizontal");
 		float ver = Input.GetAxis("Vertical");
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(transform.up * jump);
+        }
 		Vector3 playerMove = new Vector3 (hor, 0, ver) * speed * Time.deltaTime;
 		transform.Translate(playerMove, Space.Self);
 	}

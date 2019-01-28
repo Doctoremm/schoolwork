636940d5c504ddf295c90170c30a070bcd478527
Doctoremm <doctoremm@gmail.com>
2019-01-28 00:08:48 -0700

    lvl design better setup

---
+++ Models/Projects/Level Design/Level Design/Assets/Scripts/Portal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+
+	public Rigidbody rb;
+	public Transform destination;
+	void OnTriggerEnter()
+	{
+		rb.transform.position = destination.transform.position;
+	}
+
+
+}
---
+++ Models/Projects/Level Design/Level Design/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour {
+
+	public float speed = 3f;
+
+	void Update()
+	{
+		PlayerMovement();
+	}
+
+	void PlayerMovement()
+	{
+		float hor = Input.GetAxis("Horizontal");
+		float ver = Input.GetAxis("Vertical");
+		Vector3 playerMove = new Vector3 (hor, 0, ver) * speed * Time.deltaTime;
+		transform.Translate(playerMove, Space.Self);
+	}
+}

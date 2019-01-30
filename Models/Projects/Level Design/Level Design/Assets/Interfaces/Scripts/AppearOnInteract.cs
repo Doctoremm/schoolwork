4ea290b404c7107385722e4c845cc757cb9ac418
Doctoremm <doctoremm@gmail.com>
2019-01-29 20:14:12 -0700

    level interact stuff

---
+++ Models/Projects/Level Design/Level Design/Assets/Interfaces/Scripts/AppearOnInteract.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearOnInteract : MonoBehaviour, IFPSInteract {
+
+	public GameObject obj;
+	public void OnInteract()
+	{
+		if(Input.GetButtonDown("Interact"))
+		{
+		obj.gameObject.SetActive(true);
+		}
+	}
+
+}
+
---
+++ Models/Projects/Level Design/Level Design/Assets/Interfaces/Scripts/DeacativeOnInteract.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeacativeOnInteract : MonoBehaviour, IFPSInteract {
+
+	public void OnInteract()
+	{
+		if(Input.GetButtonDown("Interact"))
+		{
+			gameObject.SetActive(false);
+		}
+	}
+
+}
